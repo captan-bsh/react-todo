7d47e6b904db96891d2d9a6cc1f5d1aefb3c6735
BshCport <captan.bsh@gmail.com>
2019-04-04 23:36:30 +0430

    Updates

---
+++ precache-manifest.e3a9ef8d36eb272486690eb4ace778f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "e9c0f32f29e309f6288b",
+    "url": "/react-todo/static/js/main.f6ef2479.chunk.js"
+  },
+  {
+    "revision": "9b14f50cf7a27832620c",
+    "url": "/react-todo/static/js/2.d400c05e.chunk.js"
+  },
+  {
+    "revision": "e9c0f32f29e309f6288b",
+    "url": "/react-todo/static/css/main.e0270faf.chunk.css"
+  },
+  {
+    "revision": "e3e7b9dc7ecf7e0a20e7baf7426325f6",
+    "url": "/react-todo/index.html"
+  }
+];
